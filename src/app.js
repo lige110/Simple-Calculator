21abecd48d79d480274ad575272ce6661690f860
Meng Li <meng.li@rwth-aachen.de>
2021-06-14 15:38:51 +0200

    Updated the home page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use("/", getRoutes());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello! Feel free to test it!");
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   let err = new Error("Page Not Found");
